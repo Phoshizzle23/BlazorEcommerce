ed815a4c99f34f41f39693356b3d94faa6f12265
Phoshizzle23 <mattywoodward88@gmail.com>
2023-11-19 11:15:55 +1100

    Implemented a Service Response

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -1,5 +1,4 @@
-﻿using BlazorEcommerce.Server.Data;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers
@@ -18,10 +17,14 @@ namespace BlazorEcommerce.Server.Controllers
         
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProduct()
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct()
         {
             var products = await _context.Products.ToListAsync();
-            return Ok(products);
+            var response = new ServiceResponse<List<Product>>()
+            {
+                Data = products
+            };
+            return Ok(response);
         }
     }
 }

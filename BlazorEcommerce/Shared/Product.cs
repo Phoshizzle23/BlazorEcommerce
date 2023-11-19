2617d79756a60dc5115f8ca81f538c93b0cda98f
Phoshizzle23 <mattywoodward88@gmail.com>
2023-11-19 13:57:12 +1100

    Added Category Class to Shared

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -18,5 +18,8 @@ namespace BlazorEcommerce.Shared
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
+        public Category? Category { get; set; }
+        public int CaregoryId { get; set; }
+
     }
 }

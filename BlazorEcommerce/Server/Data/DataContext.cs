00b832fdc3b847e813b2ad4801806e17de059df6
Phoshizzle23 <mattywoodward88@gmail.com>
2023-11-20 12:48:14 +1100

    Added Product Variants for migration

--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -11,6 +11,22 @@ namespace BlazorEcommerce.Server.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<ProductVariant>()
+                .HasKey(p => new { p.ProductId, p.ProductTypeId });
+
+            modelBuilder.Entity<ProductType>().HasData(
+                    new ProductType { Id = 1, Name = "Default" },
+                    new ProductType { Id = 2, Name = "Paperback" },
+                    new ProductType { Id = 3, Name = "E-Book" },
+                    new ProductType { Id = 4, Name = "Audiobook" },
+                    new ProductType { Id = 5, Name = "Stream" },
+                    new ProductType { Id = 6, Name = "Blu-ray" },
+                    new ProductType { Id = 7, Name = "VHS" },
+                    new ProductType { Id = 8, Name = "PC" },
+                    new ProductType { Id = 9, Name = "PlayStation" },
+                    new ProductType { Id = 10, Name = "Xbox" }
+                );
+
             modelBuilder.Entity<Category>().HasData(
                 new Category
                 {
@@ -39,7 +55,6 @@ namespace BlazorEcommerce.Server.Data
                         Title = "The Hitchhiker's Guide to the Galaxy",
                         Description = "The Hitchhiker's Guide to the Galaxy is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including novels, stage shows, comic books, a 1981 TV series, a 1984 text adventure game, and 2005 feature film.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
-                        Price = 9.99m,
                         CategoryId = 1
                     },
                     new Product
@@ -48,7 +63,6 @@ namespace BlazorEcommerce.Server.Data
                         Title = "Ready Player One",
                         Description = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopia in 2045, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which would lead him to inherit the game creator's fortune. Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House). The book was published on August 16, 2011.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Ready_Player_One_cover.jpg/220px-Ready_Player_One_cover.jpg",
-                        Price = 7.99m,
                         CategoryId = 1
                     },
                     new Product
@@ -57,7 +71,6 @@ namespace BlazorEcommerce.Server.Data
                         Title = "Nineteen Eighty-Four",
                         Description = "Nineteen Eighty-Four (also published as 1984) is a dystopian novel and cautionary tale by English writer George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime. Thematically, it centres on the consequences of totalitarianism, mass surveillance and repressive regimentation of people and behaviours within society.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/5/51/1984_first_edition_cover.jpg",
-                        Price = 6.99m,
                         CategoryId = 1
                     },
                     new Product
@@ -129,11 +142,128 @@ namespace BlazorEcommerce.Server.Data
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
                     }
                 );
+
+            modelBuilder.Entity<ProductVariant>().HasData(
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 2,
+                    Price = 9.99m,
+                    OriginalPrice = 19.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 3,
+                    Price = 7.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 4,
+                    Price = 19.99m,
+                    OriginalPrice = 29.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 2,
+                    ProductTypeId = 2,
+                    Price = 7.99m,
+                    OriginalPrice = 14.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 3,
+                    ProductTypeId = 2,
+                    Price = 6.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 5,
+                    Price = 3.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 6,
+                    Price = 9.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 7,
+                    Price = 19.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 5,
+                    ProductTypeId = 5,
+                    Price = 3.99m,
+                },
+                new ProductVariant
+                {
+                    ProductId = 6,
+                    ProductTypeId = 5,
+                    Price = 2.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 8,
+                    Price = 19.99m,
+                    OriginalPrice = 29.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 9,
+                    Price = 69.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 10,
+                    Price = 49.99m,
+                    OriginalPrice = 59.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 8,
+                    ProductTypeId = 8,
+                    Price = 9.99m,
+                    OriginalPrice = 24.99m,
+                },
+                new ProductVariant
+                {
+                    ProductId = 9,
+                    ProductTypeId = 8,
+                    Price = 14.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 10,
+                    ProductTypeId = 1,
+                    Price = 159.99m,
+                    OriginalPrice = 299m
+                },
+                new ProductVariant
+                {
+                    ProductId = 11,
+                    ProductTypeId = 1,
+                    Price = 79.99m,
+                    OriginalPrice = 399m
+                }
+            );
         }
 
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        public DbSet<ProductVariant> ProductVariants { get; set; }
 
     }
 }

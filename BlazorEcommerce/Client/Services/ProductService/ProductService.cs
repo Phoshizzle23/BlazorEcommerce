a2778991b2828d11653eacf8168aad4977c65289
Phoshizzle23 <mattywoodward88@gmail.com>
2023-11-21 10:56:53 +1100

    Added Pagination for search function

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -12,6 +12,10 @@ namespace BlazorEcommerce.Client.Services.ProductService
         }
         public List<Product> Products { get; set; } = new List<Product>();
         public string Message { get; set; } = "Loading Products...";
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount { get; set; } = 0;
+
+        public string LastSearchText { get; set; } = string.Empty;
 
         public event Action ProductsChanged;
 
@@ -29,6 +33,12 @@ namespace BlazorEcommerce.Client.Services.ProductService
             if (result != null && result.Data != null)
                 Products = result.Data;
 
+            CurrentPage = 1;
+            PageCount = 0;
+
+            if(Products.Count == 0)
+                Message = "No Products Found!";
+
             ProductsChanged.Invoke();
         }
 
@@ -39,12 +49,18 @@ namespace BlazorEcommerce.Client.Services.ProductService
             return result.Data;
         }
 
-        public async Task SearchProducts(string searchText)
+        public async Task SearchProducts(string searchText, int page)
         {
+            LastSearchText = searchText;
             var result = await _http
-                .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+                .GetFromJsonAsync<ServiceResponse<ProductSearchResult>>($"api/product/search/{searchText}/{page}");
             if (result != null && result.Data != null)
-                Products = result.Data;
+            {
+                Products = result.Data.Products;
+                CurrentPage = result.Data.CurrentPage;
+                PageCount = result.Data.Pages;
+            }
+                
             if (Products.Count == 0) Message = "No products found!";
             ProductsChanged?.Invoke();
         }
